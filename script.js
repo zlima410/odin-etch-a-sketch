2f59e95d8e1d0ec7c312e75e3fb731d5dcda3b8a
zlima410 <endercraft68@gmail.com>
2024-04-28 19:29:06 -0400

    Add class name 'square' to each square in the grid

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ let squareArray = [defaultGridSize];
 
 for (let i = 0; i < defaultGridSize; i++) {
     squareArray[i] = document.createElement("div");
+    squareArray[i].classList.add("square");
     gridContainer.appendChild(squareArray[i]);
 }
