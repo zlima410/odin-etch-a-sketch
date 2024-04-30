f20927f15a985108bdac4c4c782642c55403d8f3
zlima410 <endercraft68@gmail.com>
2024-04-30 00:00:46 -0400

    Refactor script.js to create grid more efficiently and keep proper proportions

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-const gridContainer = document.querySelector(".grid-container");
-createGrid(16, 16);
+let numRowCol = 16;
 
-function createGrid(side, side) {
-    let squareArray = [side * side];
-    for (let i = 0; i < (side * side); i++) {
-        squareArray[i] = document.createElement("div");
-        squareArray[i].classList.add("square");
-        gridContainer.appendChild(squareArray[i]);
+function board() {
+    const gridContainer = document.querySelector(".grid-container");
+
+    for (row = 0; row < numRowCol; row++) {
+        const allRows = document.createElement("div");
+        allRows.className = "all-rows";
+        gridContainer.appendChild(allRows);
     }
+
+    const getAllRows = document.querySelectorAll(".all-rows");
+
+    getAllRows.forEach(eachRow => {
+        for (let i = 0; i < numRowCol; i++) {
+            eachRow.style.height = 600 / numRowCol + "px";
+
+            const square = document.createElement("div");
+            square.className = "square";
+            eachRow.appendChild(square);
+        }
+    });
 }
 
-function resetGrid() {
-    
+function clearGrid() {
+    const gridContainer = document.querySelector(".grid-container");
+    const getAllRows = document.querySelectorAll(".all-rows");
+
+    getAllRows.forEach(eachRow => {
+        gridContainer.removeChild(eachRow);
+    });
 }
-
-document.addEventListener("click", (e) => {
-    let target = e.target;
-
-    if (target.className === "reset") {
-        resetGrid();
-    }
-});
-
-document.addEventListener("mousemove", (e) => {
-    let target = e.target;
-
-    if (target.className === "square") {
-        target.setAttribute("id", "marked");
-    }
-});
